6948c458bc8e793052738cd468e48dda8da28783
ramjakaperdana <ramjakaperdana@live.com>
2023-09-21 21:34:06 +0700

    pages auth and home ready

---
+++ backend-siakad-laravel/resources/views/components/auth-footer.blade.php
@@ -0,0 +1,3 @@
+  <div class="simple-footer">
+      Copyright &copy; Jago Flutter 2023 (ramjakaperdana23)
+  </div>
---
+++ backend-siakad-laravel/resources/views/components/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <div class="footer-left">
+        Copyright &copy; 2023 <div class="bullet"></div> @ <a href="https://github.com/ramjakaperdana23">Muhamad
+            Nauval Azhar</a>
+    </div>
+    <div class="footer-right">
+        2.3.0
+    </div>
+</footer>
