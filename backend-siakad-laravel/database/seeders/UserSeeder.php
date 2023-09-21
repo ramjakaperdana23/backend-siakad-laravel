e9a408dc4106562f6c1dba156491171359a6b257
ramjakaperdana <ramjakaperdana@live.com>
2023-09-22 00:02:23 +0700

    update login,register,dashboard

---
+++ backend-siakad-laravel/database/seeders/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory(10)->create();
+
+
+
+        User::create([
+            'name'=> 'Ramjaka Admin',
+            'email'=> 'ramjaka@fic8.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('231298'),
+        ]);
+    }
+}
--- backend-siakad-laravel/resources/views/pages/auth/auth-login.blade.php
+++ backend-siakad-laravel/resources/views/pages/auth/auth-login.blade.php
@@ -16,21 +16,22 @@
 
         <div class="card-body">
             <form method="POST"
-                action="#"
+                action="{{route('login')}}"
                 class="needs-validation"
-                novalidate="">
+                novalidate=""> @csrf
                 <div class="form-group">
                     <label for="email">Email</label>
                     <input id="email"
                         type="email"
-                        class="form-control"
+                        class="form-control @error('email') is-invalid @enderror"
                         name="email"
                         tabindex="1"
-                        required
                         autofocus>
-                    <div class="invalid-feedback">
-                        Please fill in your email
-                    </div>
+                   @error('email')
+                   <div class="invalid-feedback">
+                        {{$message}}
+                </div>
+                @enderror
                 </div>
 
                 <div class="form-group">
@@ -46,13 +47,15 @@
                     </div>
                     <input id="password"
                         type="password"
-                        class="form-control"
+                        class="form-control @error('password')
+                        is-invalid @enderror"
                         name="password"
-                        tabindex="2"
-                        required>
-                    <div class="invalid-feedback">
-                        please fill in your password
-                    </div>
+                        tabindex="2" >
+                        @error('password')
+                        <div class="invalid-feedback">
+                             {{$message}}
+                     </div>
+                     @enderror
                 </div>
 
                 <div class="form-group">
@@ -75,21 +78,6 @@
                     </button>
                 </div>
             </form>
-            <div class="mt-4 mb-3 text-center">
-                <div class="text-job text-muted">Login With Social</div>
-            </div>
-            <div class="row sm-gutters">
-                <div class="col-6">
-                    <a class="btn btn-block btn-social btn-facebook">
-                        <span class="fab fa-facebook"></span> Facebook
-                    </a>
-                </div>
-                <div class="col-6">
-                    <a class="btn btn-block btn-social btn-twitter">
-                        <span class="fab fa-twitter"></span> Twitter
-                    </a>
-                </div>
-            </div>
 
         </div>
     </div>
